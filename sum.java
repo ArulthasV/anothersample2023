35769b6aa6b21d455eac8c0c61d494e0487a33b0
ArulthasV <121013270+ArulthasV@users.noreply.github.com>
2023-01-02 14:26:34 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ class Sum{
     public static void main(String args[]){
         int a = 3;
         int b = 8;
-        System.out.println(a+b);
+        System.out.println(a+b);// added a comment
         
 
     }
